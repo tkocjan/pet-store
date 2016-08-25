7fe1fc4d1ecf5b4ec7c0e00edec1366546c3fe41
waleedk <waleed_khan@ultimatesoftware.com>
2016-08-25 14:27:28 -0400

    apis

--- src/main/resources/public/view1/view1.js
+++ src/main/resources/public/view1/view1.js
@@ -1,37 +1,53 @@
 'use strict';
 
-TOA.controller('View1Ctrl', function($scope, $modal, petService) {
+TOA.controller('View1Ctrl', function ($scope, $modal, petService) {
+	$scope.init = function () {
+		$scope.pets = [];
+		$scope.loadPets();
+	}
 
-  $scope.init = function() {
-    loadPets();
-  }
+	$scope.loadPets = function () {
+		petService.getAll()
+			.then(function (response) {
+				$scope.pets = response.data;
+				console.log($scope.pets);
+				$scope.dataRetrieved = true;
+			});
+		;
+	}
 
-  $scope.loadPets = function () {
-    $scope.pets = petService.getAll();
-  }
+	$scope.add = function () {
+		var modalInstance = $modal.open({
+			templateUrl: '../view1/genericModal/genericModal.html',
+			controller: 'GenericModalController',
+			windowClass: 'settings-modal',
+			resolve: {
+				title: function () {
+					return 'page.timeOff.timeOffRequest.cancelTitle';
+				},
+				message: function () {
+					return 'page.timeOff.modal.cancel.confirmationMessage';
+				},
+				action: function () {
+					return function () {
+						return timeOffService.cancelRequest(request.id)
+					}
+				},
+				actionButtonText: function () {
+					return "page.timeOff.modal.cancel.cancel"
+				}
+			}
+		});
 
-  $scope.add = function() {
-    var modalInstance = $modal.open({
-      templateUrl: '../view1/genericModal/genericModal.html',
-      controller: 'GenericModalController',
-      windowClass: 'settings-modal',
-      resolve: {
-        title: function () { return 'page.timeOff.timeOffRequest.cancelTitle'; },
-        message: function () { return 'page.timeOff.modal.cancel.confirmationMessage'; },
-        action: function () { return function() {
-          return timeOffService.cancelRequest(request.id) } },
-        actionButtonText: function () { return "page.timeOff.modal.cancel.cancel" }
-      }
-    });
+		$scope.$on('$destroy', function () {
+			try {
+				modalInstance.close();
+			} catch (e) {
+			}
+		});
 
-    $scope.$on('$destroy', function () {
-      try {
-        modalInstance.close();
-      } catch (e) { }
-    });
-
-    modalInstance.result.then(function () {
-      $scope.loadPets();
-    });
-  };
+		modalInstance.result.then(function () {
+			$scope.loadPets();
+		});
+	};
 });
