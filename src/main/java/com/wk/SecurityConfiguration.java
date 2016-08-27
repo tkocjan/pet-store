9d82bb7072fcb32935376ea703647b1f7d6799ad
waleedk <waleed_khan@ultimatesoftware.com>
2016-08-26 22:44:29 -0400

    cleanup

--- src/main/java/com/wk/SecurityConfiguration.java
+++ src/main/java/com/wk/SecurityConfiguration.java
@@ -1,15 +1,24 @@
 package com.wk;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +29,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		// don't need security for these static resources
 		web.ignoring()
 				.antMatchers("/js/**")
 				.antMatchers("/templates/**")
@@ -62,5 +70,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.withUser("waleed").password("p").authorities(Permission.USER_PERMISSIONS)
 				.and()
 				.withUser("admin").password("p").authorities(Permission.ADMIN_PERMISSIONS);
+	}
+
+	@Component
+	public static class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+		private final ObjectMapper TOKEN_MAPPER = new ObjectMapper();
+
+		@Override
+		public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+				Authentication authentication) throws IOException, ServletException {
+
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+			TOKEN_MAPPER.writeValue(response.getWriter(), authentication);
+		}
 	}
 }
